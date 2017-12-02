a934f8a645fbb015526adddbb380cf4a35b640f5
Simon Lundmark <simon.lundmark@gmail.com>
2017-12-02 22:18:37 +0100

    Arduino: refactor headers into `tempen.h`

---
+++ cpp/tempenlib/tempen.h
@@ -0,0 +1,12 @@
+#ifndef TEMPEN_TEMPEN_H
+#define TEMPEN_TEMPEN_H
+
+#include <Led.h>
+#include <Stopwatch.h>
+#include <MicroController.h>
+#include <Blinky.h>
+#include <Sender.h>
+#include <Receiver.h>
+#include <RemoteLedProtocol.h>
+
+#endif //TEMPEN_TEMPEN_H
